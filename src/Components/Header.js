925be7161b855664a35822ea53fffaa4fa8d6cc9
taha26 <tahakhan26402@gmail.com>
2021-11-07 05:39:03 -0800

    App Bar UI And Design Completed With All Screen Responsive Supported

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Header() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar className="header-app-bar" position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Covid-19 Tracker App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
