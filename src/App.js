1f786b92d05356cda9b3dc44ec023d3f9eefce2a
taha26 <tahakhan26402@gmail.com>
2021-11-09 08:47:29 -0800

    New Component Created

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 // Importing Components
 
 import Header from "./Components/Header";
+import {GlobalStatistic} from "./Components/GlobalStatistic";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <GlobalStatistic/>
     </div>
   );
 }
