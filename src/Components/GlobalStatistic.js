9b7601f52b00673b9890bff491494a6674290c9d
taha26 <tahakhan26402@gmail.com>
2021-11-09 09:04:22 -0800

    Data Fetching Function Implemented

--- src/Components/GlobalStatistic.js
+++ src/Components/GlobalStatistic.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React,{useEffect} from "react";
 
 export const GlobalStatistic=()=>{
+
+    // useEffect(()=>{
+    //     async function fetchData(){
+
+    //         const response =await fetch("https://covidtracking.com/api/states")
+    //         console.log("response -->",response)
+    //         const data = response.json()
+    //         console.log("data -->",data)
+    //     }
+    //     fetchData()
+    // },[])
     return(
 <div>Global Statistic</div>
     );
