4efa02dc7cf8c281767c59110d6a0ec67b81754f
taha26 <tahakhan26402@gmail.com>
2021-11-12 19:24:47 +0500

    State Created For Fetching Data From API

--- src/Components/GlobalStatistic.js
+++ src/Components/GlobalStatistic.js
@@ -1,18 +1,52 @@
-import React,{useEffect} from "react";
+import React, { useEffect,useState } from "react";
 
-export const GlobalStatistic=()=>{
+// Material UI Imports
 
-    useEffect(()=>{
-         async function fetchData(){
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+
+export const GlobalStatistic = () => {
+
+    const [globalData,setGlobalData]=useState();
+
+    useEffect(() => {
+        async function fetchData() {
 
             const response = await fetch("https://corona.lmao.ninja/v2/all?yesterday")
-            console.log("response -->",response)
+            // console.log("response -->", response)
             const data = await response.json()
-            console.log("data -->",data)
+            // console.log("data -->", data)
+            setGlobalData(data);
         }
         fetchData()
-    },[])
-    return(
-<div>Global Statistic</div>
+    }, [])
+console.log("asc",globalData)
+
+    // Material UI Script
+
+    const Item = styled(Paper)(({ theme }) => ({
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    }));
+
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={3}>
+                <Grid item xs>
+                    <Item>xs</Item>
+                </Grid>
+                <Grid item xs={6}>
+                    <Item>xs=6</Item>
+                </Grid>
+                <Grid item xs>
+                    <Item>xs</Item>
+                </Grid>
+            </Grid>
+        </Box>
     );
 }
